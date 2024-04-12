9999c2ced03c418acb14a032f1533411666230c2
Hritik <hritik.com>
2024-04-12 16:47:29 -0300

    Created all the model class for put call and completed the third screen

--- app/src/main/java/com/example/myhotel/ui/fragment/GuestDetailFragment.kt
+++ app/src/main/java/com/example/myhotel/ui/fragment/GuestDetailFragment.kt
@@ -7,12 +7,13 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
 import android.widget.EditText
-import android.widget.ImageView
 import android.widget.LinearLayout
+import android.widget.RadioButton
+import android.widget.RadioGroup
 import android.widget.TextView
 import androidx.fragment.app.Fragment
-import com.bumptech.glide.Glide
 import com.example.myhotel.R
 import com.example.myhotel.data.model.Guest
 import com.example.myhotel.data.model.Hotel
@@ -20,6 +21,8 @@ import com.example.myhotel.data.model.Hotel
 class GuestDetailFragment :Fragment() {
     private lateinit var sharedPreferences: SharedPreferences
     val guestList = ArrayList<Guest>()
+    private lateinit var searchButton: Button
+    private lateinit var layoutGuests: LinearLayout
 
 
     @SuppressLint("MissingInflatedId")
@@ -40,30 +43,45 @@ class GuestDetailFragment :Fragment() {
             view.findViewById<TextView>(R.id.textCheckIn).text = checkInDate
             view.findViewById<TextView>(R.id.textCheckOut).text = checkOutDate
             view.findViewById<TextView>(R.id.textPrice).text = clickedHotel.price
-
         }
-
 
         // Add guest rows dynamically
-        val layoutGuests = view.findViewById<LinearLayout>(R.id.layoutGuests)
+        layoutGuests = view.findViewById<LinearLayout>(R.id.layoutGuests)
         for (i in 1..guest) {
             val guestRow = inflater.inflate(R.layout.guest_row, null)
-
             // Set guest name hint dynamically
             guestRow.findViewById<EditText>(R.id.editTextGuestName).hint = "Guest $i Name"
-
             layoutGuests.addView(guestRow)
         }
-
-
-
-
 
         return view
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+
+        searchButton = view.findViewById(R.id.buttonSave)
+
+
+        searchButton.setOnClickListener {
+            guestList.clear()
+
+            for (i in 0 until layoutGuests.childCount) {
+                val guestRow = layoutGuests.getChildAt(i)
+
+                val guestName = guestRow.findViewById<EditText>(R.id.editTextGuestName).text.toString()
+
+                val genderRadioGroup = guestRow.findViewById<RadioGroup>(R.id.radioGroupGender)
+                val selectedGenderRadioButtonId = genderRadioGroup.checkedRadioButtonId
+                val selectedGenderRadioButton = genderRadioGroup.findViewById<RadioButton>(selectedGenderRadioButtonId)
+                val gender = selectedGenderRadioButton?.text.toString() ?: ""
+
+                // Create a Guest object and add it to the guest list
+                val guest = Guest(guestName, gender)
+                guestList.add(guest)
+            }
+
+        }
 
     }
 }
